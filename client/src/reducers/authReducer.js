6c1b439f56a5d5d31bd4e97173b81ebd5e9eb37a
Rahul <rkredux@gmail.com>
2017-10-27 00:15:38 -0400

    Add Asycn Await and Change Reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,10 @@
+import { FETCH_USER } from "../actions/types"; 
 
-
-export default function authReducer( state = {}, action) {
-	console.log(action); 
+export default function authReducer( state = null, action) {
+	// console.log(action); 
 	switch(action.type){
+		case FETCH_USER: 
+		return action.payload || false; 
 		default: 
 		return state; 
 	}
